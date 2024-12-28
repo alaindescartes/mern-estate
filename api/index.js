3e7435a2aceea6cfad9ee84c2aebd24262d7eea4
alain descartes <ualaindescartes@gmail.com>
2024-12-27 18:08:11 -0700

    connecnt to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config({ path: "./.env" });
 const app = express();
 const PORT = 3000;
+
+mongoose
+  .connect(`${process.env.MONGO_URL}`)
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((error) => {
+    console.log(`there was an error connectin to the db:`, error);
+  });
 
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
