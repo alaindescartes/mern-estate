12daf10b4bd5f6f508500b5846811496fc252a37
alain descartes <ualaindescartes@gmail.com>
2025-01-01 21:30:22 -0700

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -69,3 +69,50 @@ export const getListing = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startindex) || 0;
+    let offer = req.query.offer;
+
+    if (offer !== undefined) {
+      offer = { $in: [true, false] };
+    }
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === false) {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === false) {
+      parking = { $in: [false, true] };
+    }
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['rent', 'sale'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      type,
+      furnished,
+      parking,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (err) {
+    next(err);
+  }
+};
