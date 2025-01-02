e165d06efb1a70ad677f1f1c24820cac35924c10
alain descartes <ualaindescartes@gmail.com>
2025-01-01 21:46:45 -0700

    update header form

--- client/src/_components/Header.jsx
+++ client/src/_components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 function Header() {
   const currentUser = useSelector(state => state.user.currentUser);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation(); // Import and use `useLocation`
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search); // Use `location.search`
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]); // Listen for changes to `location.search`
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -14,13 +34,17 @@ function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600 " />
+          <button>
+            <FaSearch className="text-slate-600 " />
+          </button>
         </form>
         <ul className="flex gap-4 ">
           <Link to="/home">
